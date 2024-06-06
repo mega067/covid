2d233772baac5765e3b659c943f770230369e246
Mega067L <angeljuares067@gmail.com>
2024-06-05 19:53:02 -0600

    animasion

--- script.js
+++ script.js
@@ -15,3 +15,24 @@ function showExtendedText(event, className) {
     link.parentNode.appendChild(link); // Mueve el enlace al final del contenedor
   }
 }
+window.addEventListener('load', () => {
+  const elementsToAnimate = document.querySelectorAll('.t1, .COVID-19, .INICIOS_DE_PANDEMIA, img'); // Seleccionar también imágenes
+
+  elementsToAnimate.forEach(element => {
+    if (element.tagName === 'IMG') {
+      // Animación para imágenes: deslizamiento desde arriba
+      element.style.opacity = 0;
+      element.style.transform = 'translateY(-20px)';
+      setTimeout(() => {
+        element.style.opacity = 1;
+        element.style.transform = 'translateY(0)';
+      }, 600); // Retraso de 500ms para las imágenes
+    } else {
+      // Animación para texto: aparición gradual
+      element.style.opacity = 0;
+      setTimeout(() => {
+        element.style.opacity = 1;
+      }, 2000); // Retraso de 1000ms para el texto
+    }
+  });
+});
